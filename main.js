b2be2227946700236a6bec37cf9878e963d18972
szantob <szantobenedek@gmail.com>
2022-05-10 23:22:02 +0200

    Request example

---
+++ main.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var request = require("request");
+
+app.get("/", function(req, res) {
+ //calling request function
+ request("https://www.google.com", function(error, response, body) {
+   if (!error && response.statusCode == 200) {
+     // writing the response to a file named data.html
+     // fs.writeFileSync("data.html", body);
+   }
+ });
+});
+
+app.listen(3000, function() {
+ console.log("Node server is running..");
+});
