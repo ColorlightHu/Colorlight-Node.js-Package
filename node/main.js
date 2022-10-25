d695d2e8c30d6103754538dc58767f2f7a82d6b0
szantob <szantobenedek@gmail.com>
2022-10-25 11:40:41 +0200

    Fixed HIL tests

--- node/main.js
+++ node/main.js
@@ -1,5 +1,9 @@
-const ColorlightConnection = require('./colorlight-node');
-const controller = new ColorlightConnection("192.168.1.37").controller;
+const Colorlight = require('./colorlight-node');
+const controller = Colorlight.connect("192.168.1.37");
+
+const activeProgram = controller.program.activeProgram
+const programList = controller.program.programList
+controller.program.activeProgram = programList[2];
 
 console.log(controller.program.activeProgram)
 console.log(controller.program.programNameList)
